fd3464ffa71e136d7a025e78d72aaf552d8bd9a3
Miguel Rios <miguelriosoliveira@gmail.com>
2026-05-30 10:10:38 +0200

    feat: Setup env parsing

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -3,6 +3,6 @@ import z from "zod";
 export const env = z
 	.object({
 		JWT_SECRET: z.string().nonempty(),
-		DATABASE_URL: z.url(),
+		DATABASE_URL: z.url().startsWith("sqlite://"),
 	})
 	.parse(process.env);
